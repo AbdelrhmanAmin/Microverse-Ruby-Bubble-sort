fc2c5b41d206c43f68fd066d3c90751f98cd3d53
AbdelrhmanAmin <maxhd213@gmail.com>
2020-11-24 18:34:05 +0200

    Bubble_sort done

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,8 +2,15 @@ def bubble_sort(arr)
   for bubble in (arr.length-1).downto(0)
     for sorter in ((arr.length-1) -bubble )
       if arr[sorter] > arr[sorter+1]
+        curr = arr[sorter] 
+        arr[sorter] = arr[sorter+1]
+        arr[sorter+1] = curr
+      end
     end
   end
+  return arr
 end
 
-bubble_sort([4,3,78,2,0,2])
+result = bubble_sort([4,3,78,2,0,2]);
+
+p result
