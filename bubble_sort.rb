d1fa93eb55af82e869a278c46cfa56ffe8395d67
AbdelrhmanAmin <maxhd213@gmail.com>
2020-11-24 18:56:37 +0200

    Add bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,3 +14,23 @@ end
 result = bubble_sort([4,3,78,2,0,2]);
 
 p result
+
+
+
+def bubble_sort_by(arr)
+  for bubble in  (arr.length-1).downto(0)
+    for sorter in 0...((arr.length-1) - bubble)
+       if yield(arr[sorter], arr[sorter+1]) > 0 
+        curr = arr[sorter];
+        arr[sorter] = arr[sorter+1]
+        arr[sorter+1] = curr
+      end
+    end
+  end
+  return arr
+end
+result2 = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
+
+p result2
