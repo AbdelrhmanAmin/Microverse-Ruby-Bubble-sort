730a58ea619d1a548a1a93446f05e2227443fbe0
AbdelrhmanAmin <maxhd213@gmail.com>
2020-11-24 21:48:33 +0200

    Fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,36 +1,36 @@
 def bubble_sort(arr)
-  for bubble in 0..(arr.length-1)
-    for sorter in 0...((arr.length-1) - bubble )
-      if arr[sorter] > arr[sorter+1]
-        curr = arr[sorter] 
-        arr[sorter] = arr[sorter+1]
-        arr[sorter+1] = curr
-      end
+  (0..(arr.length - 1)).each do |bubble|
+    (0...((arr.length - 1) - bubble)).each do |sorter|
+      next unless arr[sorter] > arr[sorter + 1]
+
+      curr = arr[sorter]
+      arr[sorter] = arr[sorter + 1]
+      arr[sorter + 1] = curr
     end
   end
-  return arr
+  arr
 end
 
-result = bubble_sort([10, 9, 8, 6, 5, 4, 3, 2, 1])
+ResultSort = bubble_sort([10, 9, 8, 6, 5, 4, 3, 2, 1])
 
-puts "Bubble sort result: #{result}"
+puts "Bubble sort result: #{ResultSort}"
 # output: [5, 6, 4, 7, 3, 8, 2, 9, 1, 10]
 # Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def bubble_sort_by(arr)
-  for bubble in 0..(arr.length-1)
-    for sorter in 0...((arr.length-1) - bubble)
-       if yield(arr[sorter], arr[sorter+1]) > 0 
-        curr = arr[sorter];
-        arr[sorter] = arr[sorter+1]
-        arr[sorter+1] = curr
-      end
+  (0..(arr.length - 1)).each do |bubble|
+    (0...((arr.length - 1) - bubble)).each do |sorter|
+      next unless yield(arr[sorter], arr[sorter + 1]).positive?
+
+      curr = arr[sorter]
+      arr[sorter] = arr[sorter + 1]
+      arr[sorter + 1] = curr
     end
   end
-  return arr
+  arr
 end
-result2 = bubble_sort_by(["hi","hello","hey"]) do |left,right|
-    left.length - right.length
+ResultSortBy = bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
 end
 
-puts "Bubble sort by result: #{result}"
+puts "Bubble sort by result: #{ResultSortBy}"
